d7d0eb57babf8323482dd8cca3fb0c2f9150fa24
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-06-15 13:22:45 +0300

    Страница сервиса LifetimeMembershipClub

---
+++ pages/lifetime_membership_club_page.py
@@ -0,0 +1,16 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from data.data import PageUrls
+
+
+class LifetimeMembershipClubPage(BasePage):
+    HEADER_TEXT = (By.TAG_NAME, 'h1')
+
+    def load(self):
+        self.open(PageUrls.LIFETIME_MEMBERSHIP_URL)
+        self.wait.wait_for_url(PageUrls.LIFETIME_MEMBERSHIP_URL)
+        return self
+
+    def get_header_text(self):
+        header_element = self.find_element(self.HEADER_TEXT)
+        return header_element.text.strip().lower()
