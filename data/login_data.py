48148bc5ff4f370210ba666c7a899aca4924ba80
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-06-24 16:32:57 +0300

    Датасет с параметрами вынесен в отдельный модуль

---
+++ data/login_data.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+@dataclass
+class LoginData:
+    username: str
+    password: str
+    description: str
+    expected: bool
+
+login_test_data = [
+            LoginData(username="angular", password="password", description="description", expected=True),
+            LoginData(username="test", password="password", description="description", expected=False),
+            LoginData(username="angular", password="test", description="description", expected=False),
+            LoginData(username="angular", password="password", description="", expected=False),
+            LoginData(username="", password="password", description="description", expected=False),
+            LoginData(username="angular", password="", description="description", expected=False),
+            LoginData(username="", password="", description="", expected=False),
+        ]
+    
+login_test_ids = [
+    "valid_fields",
+    "invalid_username",
+    "invalid_password",
+    "empty_description",
+    "empty_username",
+    "empty_password",
+    "empty_all_fields",
+    # можно добавить еще
+    # "only_username", 
+    # "only_password",
+    # "only_description",
+    # "uppercase_username",
+    # "fields_with_spaces"
+]
