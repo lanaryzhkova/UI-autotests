df983f4b666ddb8bec82bee282e52c78302aaddf
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-07-01 16:13:58 +0300

    Добавлен запуск автотестов на разных браузерах (по умолчанию chrome), с использованием grid

--- scripts/run_tests_grid.sh
+++ scripts/run_tests_grid.sh
@@ -1,10 +1,11 @@
 cd "$(dirname "$0")/.."
 
 THREADS=${1:-2}
+BROWSER=${2:-Chrome}
 
 echo "Запуск тестов, количество потоков (воркеров) $THREADS..."
 pytest -n "$THREADS" --dist=loadfile \
   --driver Remote \
   --selenium-host localhost \
   --selenium-port 4444 \
-  --capability browserName chrome
+  --capability browserName "$BROWSER"
--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -2,4 +2,6 @@ JAR="tools/selenium-server-4.44.0.jar"
 
 echo "Запуск Selenium Grid Node..."
 cd "$(dirname "$0")/.."
-java -jar "$JAR" node --hub http://localhost:4444 --port 5555
+
+java -jar "$JAR" node --hub http://localhost:4444 --port 5555 \
+    --selenium-manager true
