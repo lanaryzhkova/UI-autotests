4da91bcabd38b478e82dc40142b4568ed6f58c0f
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-07-01 13:50:18 +0300

    Добавлены фикстуры с настройками для chrome, firefox, edge, ie

--- conftest.py
+++ conftest.py
@@ -11,12 +11,29 @@ pytest_plugins = ["fixtures.customer_state_fixtures", "fixtures.auth_cookies_fix
 def chrome_options(chrome_options):
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-notifications")
-
     return chrome_options
+
+
+@pytest.fixture
+def firefox_options(firefox_options):
+    firefox_options.add_argument("--headless")
+    firefox_options.add_argument("--width=1920")
+    firefox_options.add_argument("--height=1080")
+    return firefox_options
+
+
+@pytest.fixture
+def edge_options(edge_options):
+    edge_options.add_argument("--headless=new")
+    edge_options.add_argument("--window-size=1920,1080")
+    return edge_options
+
+
+@pytest.fixture
+def ie_options(ie_options):
+    ie_options.ignore_protected_mode_settings = True
+    ie_options.ignore_zoom_level = True
+    return ie_options
 
 
 @pytest.fixture
--- scripts/run_tests.sh
+++
@@ -1,6 +0,0 @@
-cd "$(dirname "$0")/.."
-
-THREADS=${1:-2}
-
-echo "Запуск тестов, количество потоков (воркеров) $THREADS..."
-pytest -n "$THREADS" --dist=loadfile
